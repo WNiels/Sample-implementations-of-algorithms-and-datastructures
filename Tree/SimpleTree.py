a9c727a7521e274d7cba1eab83978ad00fdfc60d
WNiels <niels.westphal@stud-mail.uni-wuerzburg.de>
2017-07-27 13:34:22 +0200

    + new implementation

--- Tree/SimpleTree.py
+++ Tree/SimpleTree.py
@@ -1,41 +1,40 @@
-from itertools import chain
-
-
 class Node:
-    def __init__(self, val='def', *nodes):
-        self._children = list(nodes)
-        self._value = val
+    __slots__ = ['_elements']
 
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, val):
-        self._value = val
-
-    @value.deleter
-    def value(self):
-        self._value = None
+    def __init__(self, val, *children):
+        self._elements = [val] + list(children)
 
     @property
     def children(self):
-        return self._children
+        return self._elements[1:]
 
     @children.setter
-    def children(self, *nodes):
-        self._children = list(nodes)
+    def children(self, lst):
+        self._elements = self._elements[:1] + list(lst)
 
     @children.deleter
-    def children(self, *nodes):
-        for node in list(nodes):
-            self._children.remove(node)
+    def children(self):
+        self._elements = self._elements[:1]
 
-    def add_children(self, *nodes):
-        self._children.append(nodes)
+    @property
+    def value(self):
+        return self._elements[0]
+
+    def __repr__(self):
+        return "<{}: {} :: {} child nodes>".format(self.__class__.__name__, self.value, len(self.children))
+
+    """
+    Iterate in pre-order depth-first search order (DFS)
+    """
 
     def __iter__(self):
-        return chain(self._children)
+        yield self
+        for child in self.children:
+            yield from child
+
+    def values(self):
+        for node in self:
+            yield node.value
 
 
 if __name__ == "__main__":
@@ -43,5 +42,4 @@ if __name__ == "__main__":
     tree.children = Node('1', Node('1.1'), Node('1.2'), Node('1.3')), Node('2', Node('2.1'), Node('2.2'),
                                                                            Node('2.3')), Node('3'), Node('4')
     for node in tree:
-        print(node)
-        print(',')
+        print(node.values)
