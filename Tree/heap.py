2710b778d50edd2647c41b307a2ba9ceb0c4c295
Niels Westphal <niels.westphal@stud-mail.uni-wuerzburg.de>
2017-07-31 19:03:50 +0200

    MaxHeap implementation
    + implementet MaxHeap with core functionality

--- Tree/heap.py
+++ Tree/heap.py
@@ -1,46 +1,175 @@
-class MinHeap:
+import math
+
+
+class MaxHeap:
+    """
+    MaxHeap implementation.
+    """
     def __init__(self):
         """
-        For convenience the [0] element of the list is reserved and we start with the [1] element.
+        Constructor.
+
+        Initializes the list, backing up the heap.
         """
-        self._heap = [(None, None)]
+        self._heap = [None]
 
-    def insert(self, key, value=None):
-        self._heap.append((key, value))
-        i = len(self._heap)-1
-        self.sift_up(i)
+    def __str__(self):
+        """
+        String representation of the heap.
 
-    def remove(self):
-        # TODO: Implement
-        pass
+        Is the string representation of the list without it's first element.
 
-    def sift_up(self, index):
-        parent_index = self.get_parent_index(index)
-        if parent_index:
-            if self._heap[parent_index][0] > self._heap[index][0]:
-                parent = self._heap[parent_index]
-                self._heap[parent_index] = self._heap[index]
-                self._heap[index] = parent
-                self.sift_up(parent_index)
+        Returns
+        -------
+        str
+            String representation.
+        """
+        return str(self._heap[1:])
+
+    def insert(self, *args):
+        """
+        Ads the given arguments to the heap.
+
+        Parameters
+        ----------
+        args : comparable
+            Args to add to the heap.
+
+        """
+        self._heap.extend(args)
+        self.build_max_heap()
 
     @staticmethod
-    def get_parent_index(index):
-        parent_index = index / 2
-        if index < 1:
-            return None
-        else:
-            return parent_index
+    def left(index):
+        """
+        Calculates the left child's index.
 
-    def get_left_child(self, index):
-        left_child = 2 * index
-        if index < len(self._heap):
-            return left_child
-        else:
-            return None
+        Does not check if the index is in the heaps range!
 
-    def get_right_child(self, index):
-        right_child = 2 * index + 1
-        if index < len(self._heap):
-            return index
+        Parameters
+        ----------
+        index : int
+            Index to calculate the left child's index for.
+
+        Returns
+        -------
+        int
+            Left child's index.
+
+        """
+        return 2 * index
+
+    @staticmethod
+    def right(index):
+        """
+        Calculates the right child's index.
+
+        Does not check if the index is in the heaps range!
+
+        Parameters
+        ----------
+        index : int
+            Index to calculate the right child's index for.
+
+        Returns
+        -------
+        int
+            Right child's index.
+
+        """
+        return 2 * index + 1
+
+    @staticmethod
+    def parent(index):
+        """
+        Calculates the parents index.
+
+        Parameters
+        ----------
+        index : int
+            Index to calculate the parent's index for.
+
+        Returns
+        -------
+        int
+            Parent's index.
+
+        """
+        return math.floor(index / 2)
+
+    def max_heapify(self, index):
+        """
+        Build's local heap for a node and it's childes.
+
+        Parameters
+        ----------
+        index : int
+            Nodes index.
+
+        Returns
+        -------
+
+        """
+        l, r = self.left(index), self.right(index)
+        if l < len(self._heap) and self._heap[l] > self._heap[index]:
+            largest = l
         else:
-            return None
+            largest = index
+        if r < len(self._heap) and self._heap[r] > self._heap[largest]:
+            largest = r
+        if largest is not index:
+            self.swap(index, largest)
+            self.max_heapify(largest)
+
+    def build_max_heap(self):
+        """
+        Start's sorting the list to match heap criteria.
+
+        Working bottom up.
+
+        Returns
+        -------
+
+        """
+        i = math.floor(len(self._heap) / 2)
+        while i >= 1:
+            self.max_heapify(i)
+            i -= 1
+
+    def swap(self, first_index, second_index):
+        """
+        Swaps the position of the objects at the given indexes.
+
+        Parameters
+        ----------
+        first_index : int
+            Index of the first object to swap.
+        second_index : int
+            Index of the second object to swap.
+
+        Returns
+        -------
+
+        """
+        first_node = self._heap[first_index]
+        self._heap[first_index] = self._heap[second_index]
+        self._heap[second_index] = first_node
+
+    # TODO: Methods to implement in the future.
+
+    def lookup(self, key):
+        pass
+
+    def extract_max(self):
+        pass
+
+    def extract_min(self):
+        pass
+
+    # TODO: Unittests.
+
+
+if __name__ == '__main__':
+    max_heap = MaxHeap()
+    max_heap.insert(8, 2, 3, 9, 7, 1, 10, 16, 4, 14)
+    print(max_heap)
